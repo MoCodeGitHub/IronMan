1d595a80bdc1b675faba4fad5c48b7e1dd2ad195
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2018-07-20 17:51:26 +0200

    more routing

--- src/components/main.js
+++ src/components/main.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import LandingPage from './landingpage';
 import Aboutme from './aboutme';
+import Resume from './resume';
+import Contact from './contact';
+import Projects from './projects';
+
 
 
 const Main = () => (
   <Switch>
     <Route exact path="/" component={LandingPage} />
     <Route path="/Aboutme" component={Aboutme} />
+    <Route path="/Resume" component={Resume} />
+    <Route path="/Contact" component={Contact} />
+    <Route path="/Projects" component={Projects} />
+
 
   </Switch>
 )
