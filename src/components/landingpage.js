9126b24451577f92f93e8d216399e53e228c47ea
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2018-07-20 19:04:21 +0200

    Avatar + background

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -6,9 +6,15 @@ class Landing extends Component {
   render() {
     return (
       <div style={{width: '100', margin: 'auto'}} >
-        <grid className="Landing-grid">
-          <cell col={12}></cell>
-        </grid>
+      <Grid className="landing-grid">
+          <Cell col={12}>
+          <img
+          src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+          alt="avatar"
+          className="avatar-img"
+          />
+          </Cell>
+        </Grid>
       </div>
     )
   }
