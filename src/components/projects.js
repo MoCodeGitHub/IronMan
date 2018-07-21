4afef0fe978b62cf9072ff2743f978b5b915faab
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2018-07-21 15:27:45 +0200

    Tabs for Projects page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
+import { Tabs, Tab} from 'react-mdl';
 
 class Projects extends Component {
+  constructor (props) {
+    super (props);
+    this.state = { activeTab: 0};
+  }
   render() {
     return (
-      <div><h1>Projects Page</h1></div>
+      <div className = "Category-tabs">
+      <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
+      <Tab>React</Tab>
+      <Tab>Swift</Tab>
+      <Tab>Hyperloop</Tab>
+      <Tab>Moon Mission</Tab>
+      </Tabs>
+      </div>
     )
   }
 }
