6988dac5f79623545ae33e8d1269132d4d7e31a5
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2018-07-21 17:20:17 +0200

    Moon projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -156,6 +156,7 @@ class Projects extends Component {
         )
     } else if (this.state.activeTab === 3){
         return (
+    <div className="projects-grid">
           <Card shadow={5} style={{minWidth: '1000', margin: 'auto'}}>
           <CardTitle style={{color: '#ffff', height: '800px',background: 'url(https://cdn.pixabay.com/photo/2015/02/17/16/08/astronaut-639570_960_720.jpg) center / cover'}}>MOON MISSION</CardTitle>
           <CardText>
@@ -170,6 +171,37 @@ class Projects extends Component {
           <IconButton name="" />
           </CardMenu>
           </Card>
+
+          <Card shadow={5} style={{minWidth: '1000', margin: 'auto'}}>
+          <CardTitle style={{color: '#ffff', height: '800px',background: 'url(https://cdn.pixabay.com/photo/2015/02/17/16/08/astronaut-639570_960_720.jpg) center / cover'}}>MOON MISSION</CardTitle>
+          <CardText>
+          Apollo Missions to the Moon
+          </CardText>
+            <CardActions>
+                <Button colored>GitHub</Button>
+                <Button colored>LiveDemo</Button>
+                <Button colored>Download</Button>
+            </CardActions>
+          <CardMenu style={{color: '#ffff'}}>
+          <IconButton name="" />
+          </CardMenu>
+          </Card>
+
+          <Card shadow={5} style={{minWidth: '1000', margin: 'auto'}}>
+          <CardTitle style={{color: '#ffff', height: '800px',background: 'url(https://cdn.pixabay.com/photo/2015/02/17/16/08/astronaut-639570_960_720.jpg) center / cover'}}>MOON MISSION</CardTitle>
+          <CardText>
+          Apollo Missions to the Moon
+          </CardText>
+            <CardActions>
+                <Button colored>GitHub</Button>
+                <Button colored>LiveDemo</Button>
+                <Button colored>Download</Button>
+            </CardActions>
+          <CardMenu style={{color: '#ffff'}}>
+          <IconButton name="" />
+          </CardMenu>
+          </Card>
+    </div>
         )
     }
   }
