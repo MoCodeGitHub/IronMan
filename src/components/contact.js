fc2876aa9c392a4181d149a88b0266d4ca33ed70
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2018-07-22 17:53:00 +0200

    Avatar

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,7 +6,13 @@ class Contact extends Component {
     return (
       <div className="contact-body">
       <Grid className="contact-grid">
-      <Cell col={6}>Half page</Cell>
+      <Cell col={6}><h2>Elon Musk</h2>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Elon_Musk_2015.jpg/327px-Elon_Musk_2015.jpg"
+        alt="avatar"
+        style={{height: '250px'}}
+      />
+      </Cell>
       <Cell col={6}>Half page</Cell>
       </Grid></div>
     )
