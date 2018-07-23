cf1d98e03e23f5a773ae052bc75a209d8c85f866
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2018-07-23 18:01:39 +0200

    Set up resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Resume extends Component {
   render() {
     return (
-      <div><h1>Resume Page</h1></div>
+      <div><Grid>
+      <Cell col={4}>Left</Cell>
+      <Cell className="resume-right-col" col={4}>Right</Cell>
+      </Grid>
+      </div>
     )
   }
 }
