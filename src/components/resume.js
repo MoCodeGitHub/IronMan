45ac46c25edacf19aca341f62d6a70c96be63a0f
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2018-08-01 20:32:28 +0200

    avatar in resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -5,7 +5,15 @@ class Resume extends Component {
   render() {
     return (
       <div><Grid>
-      <Cell col={4}>Left</Cell>
+      <Cell col={4}>
+      <div style={{textAlign: 'center'}}>
+      <img
+        src= "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Elon_Musk_2015.jpg/327px-Elon_Musk_2015.jpg"
+        alt= "avatar"
+        style={{height: '200px'}}
+        />
+        </div>
+      </Cell>
       <Cell className="resume-right-col" col={8}>Right</Cell>
       </Grid>
       </div>
